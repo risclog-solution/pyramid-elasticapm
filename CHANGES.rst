efba5ce65417acc1e925480cf7278d1c5c48bc20
Sebastian Wehrmann <sebastian@wehrmann.de>
2021-04-22 13:37:15 +0200

    Preparing release 1.0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,7 +3,7 @@ Change log for pyramid-elasticapm
 =================================
 
 
-1.0.1 (unreleased)
+1.0.1 (2021-04-22)
 ==================
 
 - Be more compatible with retrieving user data from request.
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyramid-elasticapm',
-    version='1.0.1.dev0',
+    version='1.0.1',
     install_requires=[
         'elastic-apm',
         'pyramid',
